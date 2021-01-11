83a3b26d353aa994b17b26a70cdd5a36547b55cf
Jamila <jaja_mimi@hotmail.com>
2021-01-11 15:41:27 +0100

    exo while

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+// exo 1
+
+// let nbre = parseInt(prompt("choisis un nombre"));
+// let compt = 0;
+
+// switch (isNaN(nbre)) {
+//     case true:
+//         console.log(`erreur, ceci n'est pas un nombre`);
+//         break;
+//     case false:
+//         while (compt <= nbre) {
+//             console.log(compt);
+//             compt++
+//         }
+//     }
+
+console.log("______2____");
+//exo 2
+//a
+// let i = 0;
+// while (i<=10) {
+//     console.log(`tour numero ${i}`);
+//     i++;
+// }
+
+//b
+// let i = 3;
+// while (i<=6) {
+//     console.log(`tour numero ${i}`);
+//     i++;
+// }
+
+//c
+let i = 0;
+while (i<=10) {
+    console.log(`tour numero ${i}`);
+    i+=2;
+}
